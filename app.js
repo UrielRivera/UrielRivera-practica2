046f8c187ebe7a5d4950bc177fe2ed20c78f2bab
UrielRivera <74998487+UrielRivera@users.noreply.github.com>
2022-09-22 16:40:50 -0500

    Add files via upload
    feat: change app.js

---
+++ app.js
@@ -0,0 +1,11 @@
+if (navigator.serviceWorker) {
+    //identificar si estoy en  local o en github
+   // if(window.location.hostname = "localhost"){
+        console.log("Es local host");
+        navigator.serviceWorker.register('/sw.js');
+   // }else{
+     //   navigator.serviceWorker.register('./sw.js');
+    //}
+   
+}
+
